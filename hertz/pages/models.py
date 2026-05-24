7e7ed1316b6da63e12feabe9ce1a23aedf6acc77
sofiachepur <chepursofia27@ gmail.com>
2026-05-24 18:41:01 +0300

    пошта5

--- hertz/pages/models.py
+++ hertz/pages/models.py
@@ -1,7 +1,4 @@
-from django.db import models
-from django.db.models.signals import post_save, pre_save
-from django.dispatch import receiver
-from django.core.mail import send_mail
+
 import os
 import requests
 from django.db import models
