18870cd19bce3af3a9a65cc2f3333262dbc42a17
sofiachepur <chepursofia27@ gmail.com>
2026-05-24 19:12:57 +0300

    Fix cloudinary storage config2

--- hertz/hertz/settings.py
+++ hertz/hertz/settings.py
@@ -171,5 +171,12 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
