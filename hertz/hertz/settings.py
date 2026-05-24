f8a60b4561ae81489ffa71fb5d40fa8aeb95ff75
sofiachepur <chepursofia27@ gmail.com>
2026-05-24 19:20:28 +0300

    Replace ImageField with CloudinaryField3

--- hertz/hertz/settings.py
+++ hertz/hertz/settings.py
@@ -42,7 +42,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsHttpOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'hertz.urls'
